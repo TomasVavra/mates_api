670c4f9ad42df73fb347bce55560c9918f7b0087
t <tomvavra1@gmail.com>
2026-01-14 21:36:00 +0100

    saves calendar finally

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# mates_api
+1)
+install playwright python package
+Run this in your project folder:
+python3 -m venv .venv
+source .venv/bin/activate
+pip install playwright
+playwright install
+
+2) create login.txt in project folder
+username=myuser
+password=testpass
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import sync_playwright
-import time
 
 URL = "https://go.ismates.cz/mates-kk-ski-school/"
 path = "login.txt"
+
 
 def load_credentials(path):
     creds = {}
@@ -14,17 +14,17 @@ def load_credentials(path):
     return creds
 
 
-def save_calendar_html(filename="calendar.html"):
+def save_calendar_html(username, password, filename="calendar.html"):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)  # headless=False if you want to se the window
+        browser = p.chromium.launch(headless=False)     # False to see browser window
         page = browser.new_page()
 
         # 1) Open login page
         page.goto(URL, wait_until="networkidle")
 
         # 2) Fill in login and password
-        page.get_by_label("Login").fill(LOGIN)
-        page.get_by_label("Heslo").fill(PASSWORD)
+        page.get_by_label("Login").fill(username)
+        page.get_by_label("Heslo").fill(password)
 
         # 3) Press "Přihlásit se"
         page.get_by_role("button", name="Přihlásit se").click()
@@ -34,7 +34,7 @@ def save_calendar_html(filename="calendar.html"):
         page.get_by_role("button", name="Plachta - náhled").click()
         page.wait_for_load_state("networkidle")
 
-        # 5) Wait to calendar to load
+        # 5) Wait for calendar to load
         page.wait_for_selector("div.display-sgantt", timeout=15000)
 
         # 6) Load HTML of calendar
@@ -47,8 +47,8 @@ def save_calendar_html(filename="calendar.html"):
         browser.close()
         print(f"Calendar saved to {filename}")
 
+
 if __name__ == "__main__":
-    #save_calendar_html()
     creds = load_credentials(path)
 
     username = creds.get("username")
@@ -56,3 +56,5 @@ if __name__ == "__main__":
 
     print("Username:", username)
     print("Password:", password)
+
+    save_calendar_html(username, password)
