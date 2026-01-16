7d0c795ae8219f67df4fc9fecc7e2e081dd92f59
t <tomvavra1@gmail.com>
2026-01-16 21:36:57 +0100

    sends text calendar to telegram bot

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1)
-install playwright and BeautifulSoup python packages
+install playwright, BeautifulSoup, requests python packages
 Run this in your project folder:
 
 python3 -m venv .venv
@@ -7,8 +7,16 @@ source .venv/bin/activate
 pip install playwright
 playwright install
 pip install beautifulsoup4
+pip install requests
 
 
 2) create login.txt in project folder
 username=myuser
 password=testpass
+
+3) sending messages via telegram
+matesobot token
+8408797930:AAFiNFVQZAjbE7pbloXzjb63uvai3sCRynA
+https://api.telegram.org/bot8408797930:AAFiNFVQZAjbE7pbloXzjb63uvai3sCRynA/getUpdates
+chat id 8388403019
+
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import time
+import requests
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 URL = "https://go.ismates.cz/mates-kk-ski-school/"
 path = "login.txt"
-date = "1/15/26"
-#instructor = "Tomáš"
-instructor = "None"
+date = "1/16/26"
+instructor = "Zahradníček"
+# instructor = "None"
+
+# telegram bot
+TOKEN = "8408797930:AAFiNFVQZAjbE7pbloXzjb63uvai3sCRynA"
+CHAT_ID = "8388403019"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    requests.post(url, data=data)
 
 def save_html(raw_html, output_file="calendar.html"):
     with open(output_file, "w", encoding="utf-8") as f:
@@ -27,8 +37,11 @@ def save_text_only(raw_html, output_file="calendar.txt"):
     soup = BeautifulSoup(raw_html, "html.parser")
     text = soup.get_text(separator="\n", strip=True)
 
+    message = f"{date}\n\n{text}"
+    send_message(message)
+
     with open(output_file, "w", encoding="utf-8") as f:
-        f.write(text)
+        f.write(message)
 
     print("Saved clean text to", output_file)
 
@@ -41,44 +54,50 @@ def load_credentials(path):
                 creds[key] = value
     return creds
 
+def login(username, password, page):
+    # Open login page
+    page.goto(URL, wait_until="networkidle")
+
+    # Fill in login and password
+    page.get_by_label("Login").fill(username)
+    page.get_by_label("Heslo").fill(password)
+
+    # Press "Přihlásit se"
+    page.get_by_role("button", name="Přihlásit se").click()
+    page.wait_for_load_state("networkidle")
+
+def load_calendar(page):
+    # Press "Plachta - náhled"
+    page.get_by_role("button", name="Plachta - náhled").click()
+    page.wait_for_load_state("networkidle")
+
+    # Wait for calendar to load
+    page.wait_for_selector("div.display-sgantt", timeout=15000)
+
 def extract_calendar(username, password, filename="calendar.html"):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)     # False to see browser window
         page = browser.new_page()
 
-        # 1) Open login page
-        page.goto(URL, wait_until="networkidle")
+        login(username, password, page)
 
-        # 2) Fill in login and password
-        page.get_by_label("Login").fill(username)
-        page.get_by_label("Heslo").fill(password)
+        load_calendar(page)
 
-        # 3) Press "Přihlásit se"
-        page.get_by_role("button", name="Přihlásit se").click()
-        page.wait_for_load_state("networkidle")
-
-        # 4) Press "Plachta - náhled"
-        page.get_by_role("button", name="Plachta - náhled").click()
-        page.wait_for_load_state("networkidle")
-
-        # 5) Wait for calendar to load
-        page.wait_for_selector("div.display-sgantt", timeout=15000)
-
-        # 6) Fill in date
+        # Fill in date
         page.fill("input.v-datefield-textfield", date)
         page.keyboard.press("Enter")
 
-        # Fill in name filter
+        # Fill in name filter, "None" = no filter
         if (instructor != "None"):
             page.fill("input.v-textfield-tiny", instructor)
             page.keyboard.press("Enter")
 
         time.sleep(3)
 
-        # 7) Load HTML of calendar
+        # Load HTML of calendar
         html = page.locator("div.display-sgantt").inner_html()
 
-        # 8) Save to files
+        # Save to files
         save_html(html)
         save_pretty_html(html)
         save_text_only(html)
@@ -88,8 +107,8 @@ def extract_calendar(username, password, filename="calendar.html"):
 
 if __name__ == "__main__":
     creds = load_credentials(path)
-
     username = creds.get("username")
     password = creds.get("password")
 
+    # send_message("Hello from matesobot!")
     extract_calendar(username, password)
