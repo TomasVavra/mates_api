af1aac4555f0fbe779359a9249f04a2331ed654e
t <tomvavra1@gmail.com>
2026-01-16 23:52:12 +0100

    sends text calendar to telegram bot

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 URL = "https://go.ismates.cz/mates-kk-ski-school/"
-path = "login.txt"
+path_credentials = "login.txt"
+path_telegram_setings = "token.txt"
 date = "1/16/26"
 instructor = "Zahradníček"
 # instructor = "None"
@@ -13,7 +14,25 @@ instructor = "Zahradníček"
 TOKEN = "8408797930:AAFiNFVQZAjbE7pbloXzjb63uvai3sCRynA"
 CHAT_ID = "8388403019"
 
-def send_message(text):
+def load_credentials(path_credentials):
+    creds = {}
+    with open(path_credentials, "r", encoding="utf-8") as f:
+        for line in f:
+            if "=" in line:
+                key, value = line.strip().split("=", 1)
+                creds[key] = value
+    return creds
+
+def load_telegram_setings(path_telegram_setings):
+    telegram_setings = {}
+    with open(path_telegram_setings, "r", encoding="utf-8") as f:
+        for line in f:
+            if "=" in line:
+                key, value = line.strip().split("=", 1)
+                creds[key] = value
+    return telegram_setings
+
+def send_message(text, TOKEN, CHAT_ID):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": text}
     requests.post(url, data=data)
@@ -33,26 +52,17 @@ def save_pretty_html(raw_html, output_file="calendar_pretty.html"):
 
     print("Saved pretty HTML to", output_file)
 
-def save_text_only(raw_html, output_file="calendar.txt"):
+def save_text_only(raw_html, TOKEN, CHAT_ID, output_file="calendar.txt"):
     soup = BeautifulSoup(raw_html, "html.parser")
     text = soup.get_text(separator="\n", strip=True)
 
     message = f"{date}\n\n{text}"
-    send_message(message)
+    send_message(message, TOKEN, CHAT_ID)
 
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(message)
 
     print("Saved clean text to", output_file)
-
-def load_credentials(path):
-    creds = {}
-    with open(path, "r", encoding="utf-8") as f:
-        for line in f:
-            if "=" in line:
-                key, value = line.strip().split("=", 1)
-                creds[key] = value
-    return creds
 
 def login(username, password, page):
     # Open login page
@@ -74,7 +84,7 @@ def load_calendar(page):
     # Wait for calendar to load
     page.wait_for_selector("div.display-sgantt", timeout=15000)
 
-def extract_calendar(username, password, filename="calendar.html"):
+def extract_calendar(username, password, TOKEN, CHAT_ID, filename="calendar.html"):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)     # False to see browser window
         page = browser.new_page()
@@ -100,15 +110,19 @@ def extract_calendar(username, password, filename="calendar.html"):
         # Save to files
         save_html(html)
         save_pretty_html(html)
-        save_text_only(html)
+        save_text_only(html, TOKEN, CHAT_ID)
 
         browser.close()
 
 
 if __name__ == "__main__":
-    creds = load_credentials(path)
+    creds = load_credentials(path_credentials)
     username = creds.get("username")
     password = creds.get("password")
 
+    telegram_setings = load_telegram_setings(path_telegram_setings)
+    TOKEN = telegram_setings.get("TOKEN")
+    CHAT_ID = telegram_setings.get("CHAT_ID")
+
     # send_message("Hello from matesobot!")
-    extract_calendar(username, password)
+    extract_calendar(username, password, TOKEN, CHAT_ID)
